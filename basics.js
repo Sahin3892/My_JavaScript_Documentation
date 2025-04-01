f816c5846580e4b5e24024541b4b048438052557
Sahin <sahin.duruoez@gmx.de>
2025-04-01 22:28:22 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,6 @@
+// Variablen 
+
+let meineVariable = 33;
+console.log(meineVariable); // Ausgabe: 33
+meineVariable = 44; // Zuweisung einer neuen Zahl
+console.log(meineVariable);// Ausgabe: 44
---
+++ strings_numbers.js
@@ -0,0 +1,18 @@
+// Numbers
+
+let myDivision = 10/5; // Division
+let myMultiplication = 10*5; // Multiplikation
+let myAddition = 10+5; // Addition
+let mySubtraction = 10-5; // Subtraktion
+
+let myPower = 10**5; // Potenzierung
+
+// Strings
+
+let myConacatenation = "Hallo" + " " + "Welt"; // Verkettung von Strings
+console.log(myConacatenation); // Ausgabe: "Hallo Welt"
+
+// Combination of numbers and strings
+
+let myStringNumber = "10" + 5; // Verkettung von String und Zahl
+console.log(myStringNumber); // Ausgabe: "105"
