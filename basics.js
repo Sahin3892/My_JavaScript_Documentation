9de20d2a2a72dd41ce419e7c9e19ff4f3b26e644
Sahin <sahin.duruoez@gmx.de>
2025-04-11 17:31:58 +0200

    Section 1

--- basics.js
+++ basics.js
@@ -2,5 +2,21 @@
 
 let meineVariable = 33;
 console.log(meineVariable); // Ausgabe: 33
+
 meineVariable = 44; // Zuweisung einer neuen Zahl
 console.log(meineVariable);// Ausgabe: 44
+
+
+// Datentypen
+
+let myString = "Das ist ein Text"; // String
+
+let myNumber = 42; // Number
+let myFloat = 3.14; // Float
+let myInteger = 7; // Integer
+
+let myBoolean = true; // Boolean (true/false)
+
+let myArray = [1, 2, 3, 4, 5]; // Array (Liste von Werten)
+
+let myObject = { name: "Max", age: 30 }; // Objekt (Schlüssel-Wert-Paare)
