986afb2421d8a038d1f7302f7d6eb6f78e5bd77e
Sahin <sahin.duruoez@gmx.de>
2025-04-21 19:45:35 +0200

    Finished Booleans

---
+++ conditional_code.js
@@ -0,0 +1,24 @@
+let myCondition = true; // Bedingung
+
+myCondition = !myCondition; // Negation der Bedingung
+
+myCondition = false || false || true; // Oder-Bedingung
+
+myCondition = true && false && true; // Und-Bedingung
+
+myCondition = 45 == "45"; // Vergleich (gleich)
+
+myCondition = 45 === "45"; // Vergleich (identisch) --> True
+
+myCondition = 45 === "45"; // Vergleich (identisch) --> False
+
+myCondition = 45 > 123; // Vergleich (größer als)
+
+myCondition = 45 < 123; // Vergleich (kleiner als)
+
+myCondition = 45 >= 123; // Vergleich (größer oder gleich)
+
+myCondition = 45 <= 123; // Vergleich (kleiner oder gleich)
+
+
+console.log(myCondition); // Ausgabe: true/false
