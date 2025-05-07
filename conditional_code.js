3d1254dc12dbcb543f19d37d9305becc017200c0
Sahin <sahin.duruoez@gmx.de>
2025-05-07 22:02:21 +0200

    Startet with if else

--- conditional_code.js
+++ conditional_code.js
@@ -22,3 +22,9 @@ myCondition = 45 <= 123; // Vergleich (kleiner oder gleich)
 
 
 console.log(myCondition); // Ausgabe: true/false
+
+if (true){ // Wenn die Bedingung wahr ist führe den Code aus
+    console.log("Die Bedingung ist wahr.");
+} else if (!false) { // Wenn die Bedingung falsch ist führe den Code aus
+    console.log("Die Bedingung ist falsch.");
+}
